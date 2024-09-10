81027cb6384e95f8ebe7de9a28c35305c25b84af
Andrey Egorov <and2510.egorov@yandex.ru>
2024-09-10 18:40:57 +0300

    user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Subscribe, User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+        'avatar',
+    )
+    list_filter = ('email', 'username')
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,26 +1,55 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.db.models import UniqueConstraint
 
 
 class User(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [
-        'username',
-        'first_name',
-        'last_name',
-    ]
-    email = models.EmailField(
-        'email address',
-        max_length=254,
+#    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
         unique=True,
+        validators=[username_validator],
     )
+    email = models.EmailField('email', max_length=254)
+    first_name = models.CharField(verbose_name='Имя', max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=150)
+#    password = models.CharField('password', max_length=150)
     avatar = models.ImageField(blank=True, null=True)
 
-    class Meta:
+    class Meta(AbstractUser.Meta):
         ordering = ['id']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.username
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='subscriber',
+        verbose_name="Подписчик",
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='subscribing',
+        verbose_name="Автор",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            UniqueConstraint(fields=['user', 'author'],
+                             name='uniq_user_subscribe')
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.author}'
